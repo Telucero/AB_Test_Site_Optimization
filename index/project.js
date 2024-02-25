744a49c61a04c615ea7cbea4639e4f44b82f843d
Taylor Lucero <telucero@gmail.com>
2024-02-25 16:51:16 +0000

    Project file update

--- index/project.js
+++ index/project.js
@@ -1,23 +1,124 @@
 const projects = [
     {
-        name: 'Project 1',
+        name: 'Song Recommendation Program',
         description: 'Description for Project 1 goes here...',
         imageUrl: 'images/IMG_0730.jpeg',
         githubUrl: 'https://github.com/telucero/Artist_Rec'
     },
     {
-        name: 'Project 2',
+        name: 'Ensemble Analysis on Heart Failure Data',
         description: 'Description for Project 2 goes here...',
         imageUrl: 'images/AM_prior_1.jpeg',
         githubUrl: 'https://github.com/user/Ensemble_Analysis'
     },
     {
-        name: 'Project 3',
+        name: 'ETH Analysis',
         description: 'Description for Project 3 goes here...',
         imageUrl: 'images/AM_prior_R1.jpeg',
         githubUrl: 'https://github.com/user/ETH_Analysis'
-    }
-    // Add more projects as needed
+    },
+    {
+        name: 'EDOT Proposal',
+        description: 'Description for Project 3 goes here...',
+        imageUrl: 'images/AM_prior_R1.jpeg',
+        githubUrl: 'https://github.com/user/ETH_Analysis'
+    },
+    {
+        name: 'Unimaster',
+        description: 'Description for Project 3 goes here...',
+        imageUrl: 'images/AM_prior_R1.jpeg',
+        githubUrl: 'https://github.com/user/ETH_Analysis'
+    },
+    {
+        name: 'Business Development Lab',
+        description: 'Description for Project 3 goes here...',
+        imageUrl: 'images/AM_prior_R1.jpeg',
+        githubUrl: 'https://github.com/user/ETH_Analysis'
+    },
+    {
+        name: 'Sentiment Classification',
+        description: 'Description for Project 3 goes here...',
+        imageUrl: 'images/AM_prior_R1.jpeg',
+        githubUrl: 'https://github.com/user/ETH_Analysis'
+    },
+    {
+        name: 'Plant Classification Challenge',
+        description: 'Description for Project 3 goes here...',
+        imageUrl: 'images/AM_prior_R1.jpeg',
+        githubUrl: 'https://github.com/user/ETH_Analysis'
+    },
+    {
+        name: 'Space Shop Database Management',
+        description: 'Description for Project 3 goes here...',
+        imageUrl: 'images/AM_prior_R1.jpeg',
+        githubUrl: 'https://github.com/user/ETH_Analysis'
+
+    },
+    {
+        name: 'Sign Language Classification',
+        description: 'Description for Project 3 goes here...',
+        imageUrl: 'images/AM_prior_R1.jpeg',
+        githubUrl: 'https://github.com/user/ETH_Analysis'
+    },
+    {
+        name: 'Financial Self-Organizing Map (SOM) Model',
+        description: 'Description for Project 3 goes here...',
+        imageUrl: 'images/AM_prior_R1.jpeg',
+        githubUrl: 'https://github.com/user/ETH_Analysis'
+    },
+    {
+        name: 'DQN Demon Attack',
+        description: 'Description for Project 3 goes here...',
+        imageUrl: 'images/AM_prior_R1.jpeg',
+        githubUrl: 'https://github.com/user/ETH_Analysis'
+    },
+    {
+        name: 'Plant Classification Challenge',
+        description: 'Description for Project 3 goes here...',
+        imageUrl: 'images/AM_prior_R1.jpeg',
+        githubUrl: 'https://github.com/user/ETH_Analysis'
+
+    },
+    {
+        name: 'Thesis: Interpretability of XAI Across Image, Text, and Tabular Data',
+        description: 'Description for Project 3 goes here...',
+        imageUrl: 'images/AM_prior_R1.jpeg',
+        githubUrl: 'https://github.com/user/ETH_Analysis'
+    },
+    {
+        name: 'High Performance Computing on DBSCAN ',
+        description: 'Description for Project 3 goes here...',
+        imageUrl: 'images/AM_prior_R1.jpeg',
+        githubUrl: 'https://github.com/user/ETH_Analysis'
+    },
+    {
+        name: 'Q Learning : Frozen Lake',
+        description: 'Description for Project 3 goes here...',
+        imageUrl: 'images/AM_prior_R1.jpeg',
+        githubUrl: 'https://github.com/user/ETH_Analysis'
+    },
+    {
+        name: 'Airline Satisfaction',
+        description: 'Description for Project 3 goes here...',
+        imageUrl: 'images/AM_prior_R1.jpeg',
+        githubUrl: 'https://github.com/user/ETH_Analysis'
+    } /*,
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+    // Add more projects as needed*/
 ];
 
 export { projects };
