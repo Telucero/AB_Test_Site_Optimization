adbb0c4f27efaaf875336a7fc14549562131d6e2
Taylor Lucero <telucero@gmail.com>
2024-02-25 17:15:42 +0000

    update

--- index/script.js
+++ index/script.js
@@ -8,27 +8,7 @@ var swiper = new Swiper('.swiper-container', {
         prevEl: '.swiper-button-prev',
     },
 });
-/*var projects = [
-    {
-        name: 'Project 1',
-        description: 'Description for Project 1 goes here...',
-        imageUrl: 'images/IMG_0730.jpeg',
-        githubUrl: 'https://github.com/telucero/Artist_Rec'
-    },
-    {
-        name: 'Project 2',
-        description: 'Description for Project 2 goes here...',
-        imageUrl: 'images/AM_prior_1.jpeg',
-        githubUrl: 'https://github.com/user/Ensemble_Analysis'
-    },
-    {
-        name: 'Project 3',
-        description: 'Description for Project 3 goes here...',
-        imageUrl: 'images/AM_prior_R1.jpeg',
-        githubUrl: 'https://github.com/user/ETH_Analysis'
-    }
-    // Add more projects as needed
-];*/
+
 var projectDescription = document.getElementById('project-description');
 
 // Function to update project description based on the active slide
